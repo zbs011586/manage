0c0e45e03cabc699f3ebef69590e557c9ae4f7dd
zbs <zbs011586@163.com>
2019-10-14 18:27:05 +0800

    code

---
+++ src/main/java/com/manage/manage/bean/Image.java
@@ -0,0 +1,24 @@
+package com.manage.manage.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Table;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "t_image")
+public class Image {
+
+    private Integer id;
+
+    private String path;
+
+    private Integer status;
+
+    private Date createTime;
+
+}
---
+++ src/main/java/com/manage/manage/dao/ImageDao.java
@@ -0,0 +1,8 @@
+package com.manage.manage.dao;
+
+import com.manage.manage.bean.Image;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ImageDao extends Mapper<Image> {
+
+}
